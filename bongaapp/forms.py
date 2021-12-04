aca9424d8e8b21ef74d730012a69e5b54827a515
kipsang01 <enock.kipsang@student.moringaschool.com>
2021-12-04 11:53:07 +0300

    Add image form

--- bongaapp/forms.py
+++ bongaapp/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.forms.models import ModelForm
+from .models import Image,Comment
 
 
 class RegisterUserForm(UserCreationForm):
@@ -12,3 +14,10 @@ class RegisterUserForm(UserCreationForm):
     class  Meta:
         model = User
         fields = ('username','first_name','last_name','email','password1','password2')
+        
+class ImageForm(ModelForm):
+    
+    
+    class Meta:
+        model = Image
+        fields = ('image','name','caption','location' )
